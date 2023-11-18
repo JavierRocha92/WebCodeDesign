97757bd616d2aeb0bf22b1ead24b49317b454ae8
Javier Rocha Gómez <javierrg1992@gmail.com>
2023-11-18 17:49:23 +0100

    animation home and forms

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
-const button = document.getElementById('button')
-const caja = document.getElementById('caja')
+const signup = document.getElementById('signup')
+const login = document.getElementById('login')
+const login__button = document.getElementById('login__button')
+const signup__button = document.getElementById('signup__button')
+const container__forms = document.getElementById('container__forms')
 
 
-button.addEventListener('click',()=> {
-    if(caja.classList.contains('mover')){
-        caja.classList.remove('mover')
-        caja.classList.add('dejar')
-    }else{
-        caja.classList.add('mover')
-        caja.classList.remove('dejar')
-    }
-})
+
+
+const removeForm = (form_1,form_2) => {
+    form_1.classList.remove('form__rotate-appear')
+    form_1.classList.add('form__rotate-desappear')
+    setTimeout(() => {
+        form_2.classList.remove('form__rotate-desappear')
+        form_2.classList.add('form__rotate-appear')
+    },400)
+}
+const detectForm = (event) => {
+    const e = event.target
+    const form = e.parentElement.parentElement
+    if(form.id === 'signup')
+    removeForm(signup,login)
+    if(form.id === 'login')
+    removeForm(login,signup)
+}
+container__forms.addEventListener('click',detectForm)
