6c3646cb1ee7b71526c0cf1e5de9e176b4634b63
Javier Rocha Gómez <javierrg1992@gmail.com>
2023-11-25 12:04:37 +0100

    responsive done, footer left

--- js/script.js
+++ js/script.js
@@ -3,11 +3,19 @@ const login = document.getElementById('login')
 const login__button = document.getElementById('login__button')
 const signup__button = document.getElementById('signup__button')
 const container__forms = document.getElementById('container__forms')
+const menu_icon = document.getElementById('menu_icon')
+const user_icon = document.getElementById('user_icon')
+const nav = document.getElementById('nav')
 
 
 
-
-const removeForm = (form_1,form_2) => {
+/**
+ * funciton to change a form view by other depending ehich of them is on screen
+ * 
+ * @param {HTMLFormElement} form_1 
+ * @param {HTMLFormElement} form_2 
+ */
+const exhangeForms = (form_1,form_2) => {
     form_1.classList.remove('form__rotate-appear')
     form_1.classList.add('form__rotate-desappear')
     setTimeout(() => {
@@ -15,12 +23,58 @@ const removeForm = (form_1,form_2) => {
         form_2.classList.add('form__rotate-appear')
     },400)
 }
-const detectForm = (event) => {
-    const e = event.target
+/**
+ * funtion to detect which form is being pushed to call other funtions to show or hide froms
+ * 
+ * @param {event} event 
+ */
+const detectForm = (e) => {
     const form = e.parentElement.parentElement
-    if(form.id === 'signup')
-    removeForm(signup,login)
-    if(form.id === 'login')
-    removeForm(login,signup)
+    if(e.id === 'login__button' || e.id === 'signup__button'){
+        if(form.id === 'signup')
+        exhangeForms(signup,login)
+        if(form.id === 'login')
+        exhangeForms(login,signup)
+    }
 }
-container__forms.addEventListener('click',detectForm)
+
+const checkElement = (event) => {
+    const e = event.target
+    if(e.classList.contains('icon'))
+    hideForms()
+    if(e.classList.contains('form__text'))
+    detectForm(e)
+    
+}
+/**
+ * function to show container forms by update style to coatiner_form element
+ */
+const showForms = () => {
+    console.log('hola')
+    container__forms.style.display = 'flex'
+}
+/**
+ * function to hide container forms by update style to coatiner_form element
+ */
+const hideForms = () => {
+    container__forms.style.display = 'none'
+}
+
+const exchangeMenu = () => {
+    if(nav.style.display == 'none' || nav.style.display == '')
+    nav.style.display = 'block'
+    else
+    nav.style.display = 'none'
+}
+//events
+
+//event to catch a click on user icon from nav
+
+user_icon.addEventListener('click',showForms)
+
+//event to catch a click on menu icon from nav
+
+menu_icon.addEventListener('click',exchangeMenu)
+
+//events to catcha a clikck on container_forms
+container__forms.addEventListener('click',checkElement)
